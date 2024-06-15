6d4ca87ee3968b88e4606911296918d84da97ba6
Jackson Li <jacksonli_@outlook.com>
2024-06-16 01:25:41 +1000

    Ensures grid resets everytime we type a new length

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ const grid = document.querySelector("#body");
 
 const sizeInput = document.querySelector("input");
 const sizeButton = document.querySelector("button");
+const gridSize = 0;
 
 sizeButton.addEventListener('click', () => {
+    grid.textContent = '';
+    // Sets content to be empty so the grid reset
+
     const gridSize = sizeInput.value;
     sizeInput.value = '';
+    
 
-})
-
-for (i=0; i<gridSize; i++){
-    const gridRow = document.createElement("div");
-    gridRow.style.cssText = "display: flex; flex-direction: row;"
-
-    for (i=0; i<gridSize; i++){
-        const gridElement = document.createElement("div");
-        gridRow.appendChild(gridElement);
+    for (let i=0; i<gridSize; i++){
+        const gridRow = document.createElement("div");
+        gridRow.style.cssText = "display: flex; flex-direction: row;"
+    
+        for (let j=0; j<gridSize; j++){
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("gridElement");
+            gridElement.textContent = "Test";
+            gridRow.appendChild(gridElement);
+        
+        }
+        grid.appendChild(gridRow);
     }
-    grid.appendChild(gridRow);
-}
+    
+})
