ad88aef83e511c286e4d8b2b370dfbaa6d76b066
Jackson Li <jacksonli_@outlook.com>
2024-06-16 14:51:40 +1000

    Allows grid elements colour to change when mouse enters the cell

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const grid = document.querySelector("#body");
-
 const sizeInput = document.querySelector("input");
 const sizeButton = document.querySelector("button");
 const gridSize = 0;
@@ -15,9 +14,13 @@ sizeButton.addEventListener('click', () => {
         alert('Input of ' + gridSize + ' is above the maximum value of 100, try selecting a smaller value 😃');
         return;
     }
-
     const squareLength = 960 / gridSize;
+    setUpGrid(gridSize, squareLength);  
+    colourGrid()
+})
 
+function setUpGrid(gridSize,squareLength){
+    // Set up the grade based on size input
     for (let i=0; i<gridSize; i++){
         const gridRow = document.createElement("div");
         gridRow.style.cssText = "display: flex; flex-direction: row;"
@@ -26,11 +29,20 @@ sizeButton.addEventListener('click', () => {
             const gridElement = document.createElement("div");
             gridElement.classList.add("gridElement");
             gridElement.style.cssText = "width: " + squareLength + "px; height: " + squareLength + "px";
-
             gridRow.appendChild(gridElement);
-        
         }
         grid.appendChild(gridRow);
     }
+}
+
+function colourGrid() {
+    const gridElements = document.querySelectorAll(".gridElement");
+    console.log(gridElements, gridElements.length);
     
-})
+    gridElements.forEach((gridElement) => {
+        // For each grid element, we add a 'mouseenter' listener
+        gridElement.addEventListener("mouseenter", () => {
+            gridElement.style.backgroundColor = "purple";
+        });
+    })
+};
